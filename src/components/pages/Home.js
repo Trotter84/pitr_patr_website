e0f075a646d7dbad992f369e28e8c84a6c094e9c
Daniel Trotter <flare84@gmail.com>
2020-09-29 20:48:56 -0600

    soundcloud embeded

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,11 +12,33 @@ class Home extends Component {
         </div>
         <div id='homeBodyContainer'>
           <div id='gridContainer'>
-            <h1 className='title body-Fade'>New Music</h1>
-            <h1 className='title body-Fade'>Social</h1>
-            <h1 className='title body-Fade'>Updates</h1>
-            <h1 className='title body-Fade'>Store/Commuinity Feed</h1>
+            <div className='bottom-line'>
+              <h1 className='title body-Fade'>New Music</h1>
+              <div id='soundCloudLink'>
+                <iframe width="530px" height="310px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/886042678&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                <div style={{fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: '100'}}>
+                  <a href="https://soundcloud.com/itspitrpatr" title="PITRPATR" target="_blank" style={{ color: '#cccccc', textDecoration: 'none'}}>
+                    PITRPATR
+                  </a>
+                  <a href="https://soundcloud.com/itspitrpatr/sometimes-too-high" title="Sometimes Too High" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>
+                    Sometimes Too High
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='box-top-bottom bottom-line'>
+              <h1 className=' box-left-right title body-Fade'>Social</h1>
+            </div>
+            <div className='bottom-line'>
+              <h1 className='title body-Fade'>Updates</h1>
+            </div>
+            <div className='bottom-line'>
+              <h1 className='title body-Fade'>Store/Commuinity Feed</h1>
+            </div>
           </div>
+        </div>
+        <div>
+          <h1 className='title'>Contact</h1>
         </div>
       </>
     )
