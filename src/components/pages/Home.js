dcd3956894d6d29ec82be0e3db66c16541fa7024
Daniel Trotter <flare84@gmail.com>
2020-09-29 20:53:52 -0600

    soundcloud needed unique title

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
             <div className='bottom-line'>
               <h1 className='title body-Fade'>New Music</h1>
               <div id='soundCloudLink'>
-                <iframe width="530px" height="310px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/886042678&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                <iframe title='soundCloudLink' width="530px" height="310px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/886042678&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
                 <div style={{fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: '100'}}>
                   <a href="https://soundcloud.com/itspitrpatr" title="PITRPATR" target="_blank" style={{ color: '#cccccc', textDecoration: 'none'}}>
                     PITRPATR
