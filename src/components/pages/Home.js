ba264f531dae0d837583db47d13c7db1756f879b
Daniel Trotter <flare84@gmail.com>
2020-09-29 20:56:38 -0600

    soundcloud issue again

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,7 +17,7 @@ class Home extends Component {
               <div id='soundCloudLink'>
                 <iframe title='soundCloudLink' width="530px" height="310px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/886042678&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
                 <div style={{fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: '100'}}>
-                  <a href="https://soundcloud.com/itspitrpatr" title="PITRPATR" target="_blank" style={{ color: '#cccccc', textDecoration: 'none'}}>
+                  <a href="https://soundcloud.com/itspitrpatr" title="PITRPATR" target="_blank" rel="noopener noreferrer" style={{ color: '#cccccc', textDecoration: 'none'}}>
                     PITRPATR
                   </a>
                   <a href="https://soundcloud.com/itspitrpatr/sometimes-too-high" title="Sometimes Too High" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>
